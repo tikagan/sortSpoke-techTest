fece05a6502d09e82ccb48cfa7364c6a1d2a9ce1
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-02-04 16:43:45 -0500

    set up basic app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import React, {Component} from 'react';
+import Workflow from './Workflow.jsx'
+import ActionsList from './ActionsList.jsx'
 
 class App extends Component {
+  constructor(props) {
+    //pass the props to the react.component (the parent class of this component)
+    super(props);
+    //set up the default state for the application
+    this.state = {
+      'Actions':  [
+            'Import',
+            'Export',
+            'Sort',
+            'Extract',
+            'Split'
+          ]
+
+    }
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div id='sort-spoke'>
+        <nav>
+          <a href="/">Admin</a>
+          <button>Setup Workflow</button>
+        </nav>
+        <main>
+          <Workflow />
+          <ActionsList />
+        </main>
+        <div id='controller-container'>
+          <button>Save</button>
+          <button>Cancel</button>
+        </div>
+      </div>
     );
   }
 }
