5a2c4bf89ca433d2eac39d40076bec1559a47534
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-02-05 16:09:29 -0500

    hard coded test data

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,44 @@ class App extends Component {
             'Sort',
             'Extract',
             'Split'
-          ]
+          ],
+      'Workflow': {
+            '_id': 'bU4rMI7fEeeqxlvy4NOyyA==',
+          	'ProjectId': 'bOd74o7fEeeqxlvy4NOyyA==',
+          	'AcctId': 'GVb1w0skuUKO+FfzgvG+JA==',
+          	'Stages': [
+            		{
+            			'id': 1,
+            			'action': 'Import',
+            			'isStart': true,
+            			'prevStage': null,
+            			'nextStage': 2,
+            		},
+            		{
+            			'id': 2,
+            			'action': 'Sort',
+            			'isStart': false,
+            			'prevStage': 1,
+            			'nextStage': 3,
+            		},
+            		{
+            			'id': 3,
+            			'action': 'Extract',
+            			'isStart': false,
+            			'prevStage': 2,
+            			'nextStage': null,
+            		},
+            		{
+            			'id': 4,
+            			'action': 'Export',
+            			'isStart': false,
+            			'prevStage': null,
+            			'nextStage': null,
+            		}
+	          ]
+          }
 
-    }
+        }
   }
 
   render() {
