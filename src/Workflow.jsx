5493e4cd90205461027c06bd834d6621c9fa16e3
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-02-06 15:48:14 -0500

    to render function renders workflow actions from array

--- src/Workflow.jsx
+++ src/Workflow.jsx
@@ -3,11 +3,20 @@ import WorkflowAction from './WorkflowAction.jsx'
 
 class Workflow extends Component {
 
+  toRender = function () {
+    const act = this.props.workflow['Stages'].map((action) =>
+      <WorkflowAction key={action['id']} actionName={action['action']} isStart={action['isStart']} prevStage={action['prevStage']} nextStage={action['nextStage']}> </WorkflowAction>
+    )
+    return (
+      <div id="workflow">{act}</div>
+    )
+  }
 
   render() {
+    console.log('in Workflow')
     return (
-      <div id="workflow">
-
+      <div id="workflow-container">
+        {this.toRender()}
       </div>
     );
   }
