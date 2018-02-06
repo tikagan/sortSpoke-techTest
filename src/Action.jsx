816fbf5dd85312657ccb3441a0b3e0cc892db238
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-02-06 15:45:33 -0500

    renders name of action

--- src/Action.jsx
+++ src/Action.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 class Action extends Component {
-
+  constructor(props) {
+      super(props)
+    }
 
   render() {
     return (
       <div className="action">
-
+        <h3>{this.props.actionName}</h3>
       </div>
     );
   }
